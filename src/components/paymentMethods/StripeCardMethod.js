72a07d6198184728b63b09a54e75d446567c2ca9
Oskar Dabrowski <oskar.dabrowski12890@wp.pl>
2022-06-03 18:15:46 +0200

    Adding heroku host to app

--- src/components/paymentMethods/StripeCardMethod.js
+++ src/components/paymentMethods/StripeCardMethod.js
@@ -1,23 +1,25 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
-import styled from 'styled-components';
-import CheckoutForm from './CheckoutForm';
-import { useGlobalContext } from '../globalContext';
+import styled from "styled-components";
+import CheckoutForm from "./CheckoutForm";
+import { useGlobalContext } from "../globalContext";
 const StripeWrapper = styled.div`
-width: 90%;
+  width: 90%;
 `;
-const stripePromise = loadStripe("pk_test_51IfrSnFRZt7FtZ9TkBwHYG7WiX2nnzMKxEQ9TwQI7qlwjtzofZozvqXRWv4JGYo6KRbMayqiQM1DIIsfWOe5ZBze00aZqB0dnv");
+const stripePromise = loadStripe(
+  "pk_test_51IfrSnFRZt7FtZ9TkBwHYG7WiX2nnzMKxEQ9TwQI7qlwjtzofZozvqXRWv4JGYo6KRbMayqiQM1DIIsfWOe5ZBze00aZqB0dnv"
+);
 
-export default function Stripe({clientData}) {
+export default function Stripe({ clientData }) {
   const [clientSecret, setClientSecret] = useState("");
-  const {TotalCost, clientEmail} = clientData;
+  const { TotalCost, clientEmail } = clientData;
   let { ClientData } = useGlobalContext();
 
   useEffect(() => {
     // Create PaymentIntent as soon as the page loads
-    localStorage.setItem("ClientOrderData", JSON.stringify(ClientData))
-    fetch("/create-payment-intent", {
+    localStorage.setItem("ClientOrderData", JSON.stringify(ClientData));
+    fetch("https://caffe-back.herokuapp.com/create-payment-intent", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ items: TotalCost }),
@@ -27,7 +29,7 @@ export default function Stripe({clientData}) {
   }, []);
 
   const appearance = {
-    theme: 'stripe',
+    theme: "stripe",
   };
   const options = {
     clientSecret,
